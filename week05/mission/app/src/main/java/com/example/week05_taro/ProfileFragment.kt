499fab322f03799bcef2be14d0460a9c071f0f1a
YuJin Choi <cheauj@gmail.com>
2026-05-03 12:24:09 +0900

    팔로잉 RecyclerView 연결 구조

--- week05/mission/app/src/main/java/com/example/week05_taro/ProfileFragment.kt
+++ week05/mission/app/src/main/java/com/example/week05_taro/ProfileFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.week03_taro.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var followingAdapter: FollowingAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +22,26 @@ class ProfileFragment : Fragment() {
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initFollowingRecyclerView()
+    }
+
+    private fun initFollowingRecyclerView() {
+        followingAdapter = FollowingAdapter()
+
+        binding.rvFollowing.apply {
+            layoutManager = LinearLayoutManager(
+                requireContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
+            adapter = followingAdapter
+            setHasFixedSize(true)
+        }
     }
 
     override fun onDestroyView() {
