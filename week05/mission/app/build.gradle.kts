865a513033b43c2bbe1c53fa79af5bdd220f0ab1
YuJin Choi <cheauj@gmail.com>
2026-05-03 11:44:18 +0900

    Retrofit 통신을 위한 의존성 추가

--- week05/mission/app/build.gradle.kts
+++ week05/mission/app/build.gradle.kts
@@ -57,4 +57,21 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
     implementation("com.google.code.gson:gson:2.10.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // OkHttp Logging
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // Coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    // 이미지 URL 로딩
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
